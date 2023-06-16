a37ba7ad8a07315f2ff9a845736fa986032e9b8d
nehanarta <nehabaswanthrao@gmail.com>
2023-06-16 17:36:10 +0530

    Added:Employee is present or absent

--- src/com/brodglabz/Employee/EmployeeWage.java
+++ src/com/brodglabz/Employee/EmployeeWage.java
@@ -3,5 +3,12 @@ package com.brodglabz.Employee;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("welcome to employeePayroll");
+        int FULL_TIME=1;
+        int empCheck=(int)Math.floor((Math.random()*10)%2);
+        if(empCheck==1){
+            System.out.println("Employee is present");
+        }else {
+            System.out.println("Employee is absent");
+        }
     }
 }
