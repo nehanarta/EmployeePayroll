f734ca22b5597d6c03ce71d05b6ca7a9e0bc0fd3
nehanarta <nehabaswanthrao@gmail.com>
2023-06-16 17:49:51 +0530

    Added:PartTimeHrs

--- src/com/brodglabz/Employee/EmployeeWage.java
+++ src/com/brodglabz/Employee/EmployeeWage.java
@@ -4,11 +4,24 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("welcome to employeePayroll");
         int FULL_TIME=1;
-        int empCheck=(int)Math.floor((Math.random()*10)%2);
+        int PART_TIME=2;
+        int EMP_RATE_PER_HOUR=40;
+        int empHrs=0;
+        int empTotalWage=0;
+        int empCheck=(int)Math.floor((Math.random()*10)%3);
         if(empCheck==1){
-            System.out.println("Employee is present");
-        }else {
+            empHrs=8;
+            System.out.println("Employee is FullTimeHrs");
+        }else if(empCheck==2){
+            System.out.println("Employee is partTimeHrs");
+            empHrs=4;
+        }
+        else {
+            empHrs=0;
             System.out.println("Employee is absent");
         }
+        empTotalWage=empHrs*EMP_RATE_PER_HOUR;
+        System.out.println("EmpWage====>"+empTotalWage);
     }
+
 }
