52972f15ee3151db3b38b0aba85a11aa9e85eb30
nehanarta <nehabaswanthrao@gmail.com>
2023-06-16 18:15:32 +0530

    Added: empwage with switch case

--- src/com/brodglabz/Employee/EmployeeWage.java
+++ src/com/brodglabz/Employee/EmployeeWage.java
@@ -4,26 +4,27 @@ public class EmployeeWage {
     public  static final int FULL_TIME=1;
     public static final int PART_TIME=2;
     public static final int EMP_RATE_PER_HOUR=40;
-   public static final  int NUM_OF_WORKING_DAYS=20;
     public static void main(String[] args) {
         int empHrs = 0;
-        int empTotalWage = 0;
         int empWage = 0;
-        for (int Day = 1; Day <= NUM_OF_WORKING_DAYS; Day++) {
             int empCheck = (int) Math.floor((Math.random() * 10) % 3);
-            if (empCheck == 1) {
-                empHrs = 8;
-
-            } else if (empCheck == 2) {
-                empHrs = 4;
-            } else {
-                empHrs = 0;
-
+            switch(empCheck) {
+                case 0:
+                    empHrs = 0;
+                    break;
+                case 1:
+                    empHrs = 8;
+                    break;
+                case 2:
+                    empHrs = 4;
+                    break;
+                default:
+                    break;
             }
             empWage = EMP_RATE_PER_HOUR * empHrs;
-            empTotalWage += empWage;
-        }
-            System.out.println("EmpWage====>" + empTotalWage);
+
+
+            System.out.println("EmpWage====>" + empWage);
         }
     }
 
