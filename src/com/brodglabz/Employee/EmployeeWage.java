d9dda8679f40881da72b1335127cd3e0c5f9e7bb
nehanarta <nehabaswanthrao@gmail.com>
2023-06-16 18:08:58 +0530

    Added:calculate Num of workings Day

--- src/com/brodglabz/Employee/EmployeeWage.java
+++ src/com/brodglabz/Employee/EmployeeWage.java
@@ -1,27 +1,30 @@
 package com.brodglabz.Employee;
 
 public class EmployeeWage {
+    public  static final int FULL_TIME=1;
+    public static final int PART_TIME=2;
+    public static final int EMP_RATE_PER_HOUR=40;
+   public static final  int NUM_OF_WORKING_DAYS=20;
     public static void main(String[] args) {
-        System.out.println("welcome to employeePayroll");
-        int FULL_TIME=1;
-        int PART_TIME=2;
-        int EMP_RATE_PER_HOUR=40;
-        int empHrs=0;
-        int empTotalWage=0;
-        int empCheck=(int)Math.floor((Math.random()*10)%3);
-        if(empCheck==1){
-            empHrs=8;
-            System.out.println("Employee is FullTimeHrs");
-        }else if(empCheck==2){
-            System.out.println("Employee is partTimeHrs");
-            empHrs=4;
+        int empHrs = 0;
+        int empTotalWage = 0;
+        int empWage = 0;
+        for (int Day = 1; Day <= NUM_OF_WORKING_DAYS; Day++) {
+            int empCheck = (int) Math.floor((Math.random() * 10) % 3);
+            if (empCheck == 1) {
+                empHrs = 8;
+
+            } else if (empCheck == 2) {
+                empHrs = 4;
+            } else {
+                empHrs = 0;
+
+            }
+            empWage = EMP_RATE_PER_HOUR * empHrs;
+            empTotalWage += empWage;
         }
-        else {
-            empHrs=0;
-            System.out.println("Employee is absent");
+            System.out.println("EmpWage====>" + empTotalWage);
         }
-        empTotalWage=empHrs*EMP_RATE_PER_HOUR;
-        System.out.println("EmpWage====>"+empTotalWage);
     }
 
-}
+
