ff49efbb188ffdf8ebf0b7d596f8863722c0a9cd
nehanarta <nehabaswanthrao@gmail.com>
2023-07-03 18:18:47 +0530

    Added:calculate wage on condition

--- src/com/brodglabz/Employee/EmployeeWage.java
+++ src/com/brodglabz/Employee/EmployeeWage.java
@@ -1,31 +1,57 @@
 package com.brodglabz.Employee;
 
 public class EmployeeWage {
-    public  static final int FULL_TIME=1;
-    public static final int PART_TIME=2;
-    public static final int EMP_RATE_PER_HOUR=40;
+    static int finalWage = 0;
+    static int finalWorkHrs = 0;
     public static void main(String[] args) {
+        System.out.println("Employee Wage Calculation");
+        int totalWage = 0;
+        int perHourWage = 20;
+        int fullDayWorkHrs = 8;
+        int partTimeWorkHrs = 4;
+        int totalWorkingDaysInMonth = 20;
         int empHrs = 0;
-        int empWage = 0;
-            int empCheck = (int) Math.floor((Math.random() * 10) % 3);
-            switch(empCheck) {
-                case 0:
-                    empHrs = 0;
-                    break;
+
+        for(int i=0; i<totalWorkingDaysInMonth; i++) {
+            int isPresent = (int) (Math.random() * 10) % 3;
+            switch (isPresent) {
                 case 1:
+                    System.out.println("Emp is Full day present");
+                    totalWage = (fullDayWorkHrs * perHourWage);
                     empHrs = 8;
                     break;
                 case 2:
+                    System.out.println("Emp doing part time");
+                    totalWage = (partTimeWorkHrs * perHourWage);
                     empHrs = 4;
                     break;
+                case 0:
+                    System.out.println("Emp is absent");
+                    totalWage = 0;
+                    empHrs = 0;
+                    break;
                 default:
+                    System.out.println("!!!Something wrong!!!");
                     break;
             }
-            empWage = EMP_RATE_PER_HOUR * empHrs;
+            finalWage = finalWage + totalWage;
+            System.out.println("FINAL WAGE = " + finalWage);
 
+            finalWorkHrs = finalWorkHrs + empHrs;
+            System.out.println("FINAL WORK HRS = " + finalWorkHrs);
 
-            System.out.println("EmpWage====>" + empWage);
+            if(finalWorkHrs == 100){
+                System.out.println("EMP COMPLETED HIS WORK HRS OF THIS MONTH");
+                break;
+            } else if (finalWorkHrs > 100) {
+                System.out.println("!Work Hrs Exceeds the limit! Reset");
+                finalWage = finalWage - totalWage;
+                System.out.println("Final wage --> " +finalWage);
+
+                finalWorkHrs = finalWorkHrs - empHrs;
+                System.out.println("Final work hrs --> " +finalWorkHrs);
+            }
         }
+        System.out.println("FINAL WAGE = " + finalWage);
     }
-
-
+}
